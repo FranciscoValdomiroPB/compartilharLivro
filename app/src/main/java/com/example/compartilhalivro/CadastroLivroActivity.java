8a10a3d4d9ab9fdc428d9ca5dfbff12deaccffd4
FRANCISCO VALDOMIRO PINTO BEZERRA <202104564848@alunos.estacio.br>
2023-09-27 21:30:00 -0300

    Primeiro commit em tela de cadastro de Livros

---
+++ app/src/main/java/com/example/compartilhalivro/CadastroLivroActivity.java
@@ -0,0 +1,14 @@
+package com.example.compartilhalivro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastroLivroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_livro);
+    }
+}
